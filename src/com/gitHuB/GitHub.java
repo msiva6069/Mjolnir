6db5909844a3d44300837bef762671c072db4fba
msiva6069 <msiva6069@gmail.com>
2022-03-27 12:42:30 +0530

    Water

---
+++ src/com/gitHuB/GitHub.java
@@ -0,0 +1,14 @@
+package com.gitHuB;
+
+public class GitHub {
+	public static void main(String[] args) {
+		String s=("Hellow Java");
+		int count=0;
+		for (int i = 0; i < s.length(); i++) {
+			System.out.println(s.charAt(i));
+			count++;	
+		}
+		System.out.println("String Count  =  "+count);
+	}
+
+}
